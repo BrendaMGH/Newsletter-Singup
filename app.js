fe286d29fdfd85caebea33bda25300ee2b390ffe
Brenda <bmgonzalezh@uqvirtual.edu.co>
2020-08-19 18:22:28 -0500

    Use of APIs and Heroku (section 20)

---
+++ app.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const { post } = require("request");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    const firstName= req.body.firstName;
+    const lastName= req.body.lastName;
+    const email= req.body.email;
+    
+    const data={
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url="https://us17.api.mailchimp.com/3.0/lists/"+process.env.API_ID;
+    const properties={
+        method: "POST",
+        auth: "Brenda:"+process.env.API_KEY
+    };
+    const request = https.request(url,properties,function(response){
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/success",function(req,res){
+    res.redirect("/");
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server running in port 3000");
+});
